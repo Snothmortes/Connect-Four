f7d84086517df9d436987593fbfcaea955692574
glen <glen.jakobsen@gmail.com>
2020-12-25 11:02:00 +0100

    Add project files.

---
+++ Connect Four/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Connect_Four
+{
+    class Program
+    {
+        private static List<string> myList = new List<string>()
+           {
+            "A_Red",
+            "B_Yellow",
+            "A_Red",
+            "B_Yellow",
+            "A_Red",
+            "B_Yellow",
+            "G_Red",
+            "B_Yellow",
+        };
+
+        static void Main(string[] args)
+        {
+            var _ = ConnectFour.WhoIsWinner(myList);
+        }
+    }
+}
